1e609f5d7b1ce524ac9c84b4ce9eb41ef8065f90
Denilson das Mercês Amorim <denimorim@gmail.com>
2022-08-25 11:05:35 -0300

    add background / jvm / jvmti

---
+++ src/listing/example-jvmti.cpp
@@ -0,0 +1,38 @@
+#include <jvmti.h>
+
+JNIEXPORT jint JNICALL
+Agent_OnLoad(JavaVM *vm,
+             char *options,
+             void *reserved)
+{
+    jvmtiError err;
+    jvmtiEnv *jvmti;
+
+    vm->GetEnv((void **)&jvmti, JVMTI_VERSION);
+
+    jvmtiCapabilities caps;
+    memset(&caps, 0, sizeof(caps));
+    caps.can_generate_method_entry_events = true;
+    jvmti->AddCapabilities(&caps);
+
+    jvmtiEventCallbacks callbacks;
+    memset(&callbacks, 0, sizeof(callbacks));
+    callbacks.MethodEntry = MethodEntry;
+
+    jvmti->SetEventNotificationMode(
+            JVMTI_ENABLE, JVMTI_EVENT_METHOD_ENTRY, NULL);
+    jvmti->SetEventCallbacks(&callbacks, sizeof(callbacks));
+
+    return 0;
+}
+
+void JNICALL
+MethodEntry(jvmtiEnv *jvmti_env,
+            JNIEnv *jni_env,
+            jthread thread,
+            jmethodID method)
+{
+    printf("method %p called by thread %p\n",
+            (void*) method, (void*) thread);
+}
+
